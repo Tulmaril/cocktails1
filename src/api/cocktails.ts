2f41c05386cdfb16430e52e24d993368af1ae1ef
asushakov <sh98889@gmail.com>
2025-04-02 19:18:23 +0700

    remove error simulation

--- src/api/cocktails.ts
+++ src/api/cocktails.ts
@@ -4,9 +4,6 @@ export const fetchCocktailByName = async (param: string, signal?: AbortSignal): 
   try {
     const response: CocktailsResponse = await fetch(`${import.meta.env.VITE_APP_API_URL}/search.php?s=${param}`, { signal })
 
-    if (Math.random() < 0.1) {
-      throw new Error(`Error. This error simulates error. 10% channce, try again`);
-    }
     return await response.json()
   } catch (error) {
     if (error instanceof Error) {
